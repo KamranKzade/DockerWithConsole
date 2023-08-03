73b6b227987876d4a808bf4b6113f0254b5c47a7
Kamran <311.kamran@gmail.com>
2023-08-03 17:37:04 +0400

    Add project files.

---
+++ ConsoleDocker/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int i = 1;
+
+		while (i < 100000)
+		{
+			Console.WriteLine("Hello World " + i);
+			i++;
+			Thread.Sleep(1000);
+		}
+	}
+}
